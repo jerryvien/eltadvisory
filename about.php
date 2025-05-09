700dfba29844f676c671bdd29a026f58a6d8aae8
jerryvien <jerryyee0902@gmail.com>
2025-05-09 09:57:43 +0800

    done update header foter

---
+++ about.php
@@ -0,0 +1,40 @@
+<?php include 'header.php'; ?>
+
+<main class="main">
+
+  <!-- Page Title -->
+  <div class="page-title" data-aos="fade">
+    <div class="container">
+      <nav class="breadcrumbs">
+        <ol>
+          <li><a href="index.php">Home</a></li>
+          <li class="current">About</li>
+        </ol>
+      </nav>
+      <h1>About</h1>
+    </div>
+  </div>
+
+  <!-- About Section -->
+  <section id="about" class="section about">
+    <div class="container" data-aos="fade-up" data-aos-delay="100">
+      <div class="row gy-4">
+        <div class="col-lg-6 order-1 order-lg-2">
+          <img src="assets/img/about.jpg" class="img-fluid" alt="">
+        </div>
+        <div class="col-lg-6 order-2 order-lg-1 content">
+          <h3>Voluptatem dignissimos provident</h3>
+          <p class="fst-italic">Lorem ipsum dolor sit amet...</p>
+          <ul>
+            <li><i class="bi bi-check2-all"></i> Ullamco laboris nisi ut aliquip...</li>
+            <li><i class="bi bi-check2-all"></i> Duis aute irure dolor...</li>
+          </ul>
+          <p>Ullamco laboris nisi ut aliquip ex ea commodo consequat...</p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+</main>
+
+<?php include 'footer.php'; ?>
