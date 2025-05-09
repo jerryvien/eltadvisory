9f4f2c8f9fdd8b0053fdfa00399b0df3e15a42ab
jerryvien <jerryyee0902@gmail.com>
2025-05-09 10:06:05 +0800

    update header

--- header.php
+++ header.php
@@ -62,12 +62,9 @@
 
     <nav id="navmenu" class="navmenu">
       <ul>
-        <li><a href="index.html" class="active">Home</a></li>
-        <li><a href="about.html">About</a></li>
-        <li><a href="services.html">Services</a></li>
-        <li><a href="portfolio.html">Portfolio</a></li>
-        <li><a href="team.html">Team</a></li>
-        <li><a href="pricing.html">Pricing</a></li>
+        <li><a href="index.php" class="active">Home</a></li>
+        <li><a href="about.php">About</a></li>
+        <li><a href="services.php">Services</a></li>
         <li><a href="blog.html">Blog</a></li>
         <li><a href="contact.html">Contact</a></li>
       </ul>
